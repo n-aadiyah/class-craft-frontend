99723143a762a9b2f95be1f53a6fdcb353b2110b
naadiya sakkeer <naadiyasakkeer@gmail.com>
2025-10-28 12:30:04 +0530

    authentication working complition

--- src/App.js
+++ src/App.js
@@ -10,13 +10,34 @@ import RewardSystem from "./pages/Admin/RewardSystem";
 import TeacherSettings from "./pages/Admin/TeacherSettings";
 import TeacherDashboard from "./pages/Admin/TeacherDashboard";
 import TrackPerformance from "./pages/Admin/TrackPerformance";
-
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import AdminDashboard from "./pages/AdminDashboard";
+import StudentDashboard from "./pages/StudentDashboard";
+import ProtectedRoute from "./components/ProtectedRoute";
 function App() {
   return (
     <Routes>
       {/* Public Page */}
       <Route path="/" element={<LandingPage />} />
-
+ <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+         <Route
+          path="/AdminDashboard"
+          element={
+            <ProtectedRoute role="admin">
+              <AdminDashboard />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/student-dashboard"
+          element={
+            <ProtectedRoute role="student">
+              <StudentDashboard />
+            </ProtectedRoute>
+          }
+        />
       {/* Teacher Dashboard Pages */}
       <Route path="/teacher/dashboard"element={<TeacherDashboardLayout><TeacherDashboard /></TeacherDashboardLayout>}/>
       <Route path="/teacher/classes"element={<TeacherDashboardLayout><ManageClasses /></TeacherDashboardLayout>}/>
